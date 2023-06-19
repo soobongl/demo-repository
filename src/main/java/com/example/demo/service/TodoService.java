3b631732922a03edbc0cc489748fe8fc5700926d
Lee Soo Yeon <lala555@naver.com>
2023-06-19 22:09:29 +0900

    18. @Query 사용하기

--- src/main/java/com/example/demo/service/TodoService.java
+++ src/main/java/com/example/demo/service/TodoService.java
@@ -2,7 +2,6 @@ package com.example.demo.service;
 
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
-
 import com.example.demo.model.TodoEntity;
 import com.example.demo.persistence.TodoRepository;
 
@@ -14,11 +13,12 @@ public class TodoService {
 	
 	public String testService(){
 		// Todo Entity 생성
-		TodoEntity entity = TodoEntity.builder().userId("user01").title("My first todo item").build();
+		TodoEntity entity = TodoEntity.builder().userId("Lee Soo Yeon").title("My first todo item").build();
 		// Todo Entity 저장
 		repository.save(entity);
 		// Todo Entity 검색
 		TodoEntity savedEntity = repository.searchByUserId(entity.getUserId()).get(0);
 		return savedEntity.getUserId();
+
 	}
 }
